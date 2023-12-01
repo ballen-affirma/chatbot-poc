f7fd06bfb4d5cf20cde5ea7ce91d72dc314e35a1
Benjamin Allen <ballen@affirma.com>
2023-12-01 12:27:54 -0600

    Set up API deployment to GCP

--- webapi/main.py
+++ webapi/main.py
@@ -1,4 +1,5 @@
 import json
+import os
 import call_chat_bison
 from flask import Flask, request, jsonify
 
@@ -16,3 +17,6 @@ def handleRequest():
     response.headers.add('Access-Control-Allow-Origin', '*')
     print(text)
     return response
+
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
